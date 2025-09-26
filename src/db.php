9714fc7bef5dc8d5a780cee391cd44914091f034
AugustoIZO <augusto.iocco@institutozonaoeste.edu.ar>
2025-09-26 10:22:51 -0300

    Faltan get y post del sistema

--- src/db.php
+++ src/db.php
@@ -9,6 +9,7 @@ $dbname = $_ENV['DB_NAME'] ?? 'ALISBOOK_BD';
 $user   = $_ENV['DB_USER'] ?? 'root';
 $pass   = $_ENV['DB_PASS'] ?? '';
 
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
